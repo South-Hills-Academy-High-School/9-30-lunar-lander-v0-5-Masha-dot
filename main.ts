aab2094c4de7117e6929d0a70e6607572eecd625
Masha-dot <113388272+Masha-dot@users.noreply.github.com>
2022-09-30 14:24:22 -0700

    working

--- main.ts
+++ main.ts
@@ -3,26 +3,65 @@ namespace SpriteKind {
     export const rocketengine = SpriteKind.create()
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    apple.ay = 25 * Math.sin(angle)
-    apple.ax = 25 * Math.cos(angle)
+    boom.ay = 25 * Math.sin(angle)
+    boom.ax = 25 * Math.cos(angle)
     fireball.setFlag(SpriteFlag.Invisible, false)
+    focketfleg = 1
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     angle += 15 * (3.14 / 180)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, function (sprite, location) {
-    apple.setVelocity(0, -1)
+    if (Math.abs(boom.vx) > 20 || boom.vy > 50) {
+        boom = sprites.create(img`
+            . . . . 2 2 2 2 2 2 2 2 . . . . 
+            . . . 2 4 4 4 5 5 4 4 4 2 2 2 . 
+            . 2 2 5 5 d 4 5 5 5 4 4 4 4 2 . 
+            . 2 4 5 5 5 5 d 5 5 5 4 5 4 2 2 
+            . 2 4 d d 5 5 5 5 5 5 d 4 4 4 2 
+            2 4 5 5 d 5 5 5 d d d 5 5 5 4 4 
+            2 4 5 5 4 4 4 d 5 5 d 5 5 5 4 4 
+            4 4 4 4 . . 2 4 5 5 . . 4 4 4 4 
+            . . b b b b 2 4 4 2 b b b b . . 
+            . b d d d d 2 4 4 2 d d d d b . 
+            b d d b b b 2 4 4 2 b b b d d b 
+            b d d b b b b b b b b b b d d b 
+            b b d 1 1 3 1 1 d 1 d 1 1 d b b 
+            . . b b d d 1 1 3 d d 1 b b . . 
+            . . 2 2 4 4 4 4 4 4 4 4 2 2 . . 
+            . . . 2 2 4 4 4 4 4 2 2 2 . . . 
+            `, SpriteKind.Player)
+        boom.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y))
+        scene.cameraFollowSprite(boom)
+        music.bigCrash.playUntilDone()
+        pause(100)
+        game.over(false)
+    } else {
+        if (landingFlag == 0) {
+            fuel += 100
+            landingFlag = 1
+        }
+    }
+    boom.setVelocity(0, -1)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     angle += -15 * (3.14 / 180)
 })
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    apple.ay = 20
+    boom.ay = 20
     fireball.setFlag(SpriteFlag.Invisible, true)
+    rocketflag = 0
 })
-let apple: Sprite = null
+let focketfleg = 0
+let boom: Sprite = null
 let angle = 0
 let fireball: Sprite = null
+let landingFlag = 0
+let rocketflag = 0
+let fuel = 1000
+let fuelSprite = textsprite.create(convertToText(fuel))
+rocketflag = 0
+landingFlag = 1
 fireball = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 4 4 4 4 . . . . . . 
@@ -45,7 +84,7 @@ fireball.setFlag(SpriteFlag.Invisible, true)
 angle = 0
 tiles.setCurrentTilemap(tilemap`level2`)
 effects.clouds.startScreenEffect()
-apple = sprites.create(img`
+boom = sprites.create(img`
     . . . . . . . e c 7 . . . . . . 
     . . . . e e e c 7 7 e e . . . . 
     . . c e e e e c 7 e 2 2 e e . . 
@@ -67,17 +106,26 @@ let engine = sprites.create(img`
     7 3 
     3 7 
     `, SpriteKind.rocketengine)
-scene.cameraFollowSprite(apple)
-scaling.scaleByPercent(apple, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
-apple.ay = 20
+scene.cameraFollowSprite(boom)
+scaling.scaleByPercent(boom, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+boom.ay = 20
 let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
 let minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
 game.onUpdate(function () {
+    fuelSprite.destroy()
     minimap2.destroy()
+    fuelSprite = textsprite.create(convertToText(fuel))
+    fuelSprite.setPosition(boom.x, boom.top)
     myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
-    minimap.includeSprite(myMinimap, apple, MinimapSpriteScale.MinimapScale)
+    minimap.includeSprite(myMinimap, boom, MinimapSpriteScale.MinimapScale)
     minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
-    minimap2.setPosition(apple.x - 50, apple.y - 30)
-    engine.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
-    fireball.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
+    minimap2.setPosition(boom.x - 50, boom.y - 30)
+    engine.setPosition(boom.x + -8 * Math.cos(angle), boom.y + -8 * Math.sin(angle))
+    fireball.setPosition(boom.x + -8 * Math.cos(angle), boom.y + -8 * Math.sin(angle))
+    if (focketfleg == 1) {
+        fuel += -2
+        if (boom.y < 400) {
+            landingFlag = 0
+        }
+    }
 })
