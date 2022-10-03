8117debf1f33307ad336d2836f64e5085f44b1c6
Masha-dot <113388272+Masha-dot@users.noreply.github.com>
2022-10-03 13:33:16 -0700

    game

--- main.ts
+++ main.ts
@@ -2,17 +2,8 @@ namespace SpriteKind {
     export const map = SpriteKind.create()
     export const rocketengine = SpriteKind.create()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    boom.ay = 25 * Math.sin(angle)
-    boom.ax = 25 * Math.cos(angle)
-    fireball.setFlag(SpriteFlag.Invisible, false)
-    focketfleg = 1
-})
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    angle += 15 * (3.14 / 180)
-})
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, function (sprite, location) {
-    if (Math.abs(boom.vx) > 20 || boom.vy > 50) {
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (sprite, location) {
+    if (Math.abs(apple.vx) > 20 || apple.vy > 50) {
         boom = sprites.create(img`
             . . . . 2 2 2 2 2 2 2 2 . . . . 
             . . . 2 4 4 4 5 5 4 4 4 2 2 2 . 
@@ -30,12 +21,11 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, fun
             . . b b d d 1 1 3 d d 1 b b . . 
             . . 2 2 4 4 4 4 4 4 4 4 2 2 . . 
             . . . 2 2 4 4 4 4 4 2 2 2 . . . 
-            `, SpriteKind.Player)
-        boom.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y))
+            `, SpriteKind.Projectile)
         scene.cameraFollowSprite(boom)
         music.bigCrash.playUntilDone()
-        pause(100)
-        game.over(false)
+        gameOverFlag += 1
+        boom.setPosition(apple.x, apple.y)
     } else {
         if (landingFlag == 0) {
             fuel += 100
@@ -44,24 +34,36 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, fun
     }
     boom.setVelocity(0, -1)
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    boom.ay = 25 * Math.sin(angle)
+    boom.ax = 25 * Math.cos(angle)
+    fireball.setFlag(SpriteFlag.Invisible, false)
+    focketfleg = 1
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    angle += 15 * (3.14 / 180)
+})
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     angle += -15 * (3.14 / 180)
 })
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    boom.ay = 20
+    apple.ay = 20
     fireball.setFlag(SpriteFlag.Invisible, true)
     rocketflag = 0
 })
 let focketfleg = 0
 let boom: Sprite = null
+let apple: Sprite = null
 let angle = 0
 let fireball: Sprite = null
 let landingFlag = 0
 let rocketflag = 0
+tiles.setCurrentTilemap(tilemap`level3`)
+let gameOverFlag = 0
 let fuel = 1000
 let fuelSprite = textsprite.create(convertToText(fuel))
 rocketflag = 0
-landingFlag = 1
+landingFlag = 0
 fireball = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 4 4 4 4 . . . . . . 
@@ -82,9 +84,8 @@ fireball = sprites.create(img`
     `, SpriteKind.Projectile)
 fireball.setFlag(SpriteFlag.Invisible, true)
 angle = 0
-tiles.setCurrentTilemap(tilemap`level2`)
 effects.clouds.startScreenEffect()
-boom = sprites.create(img`
+apple = sprites.create(img`
     . . . . . . . e c 7 . . . . . . 
     . . . . e e e c 7 7 e e . . . . 
     . . c e e e e c 7 e 2 2 e e . . 
@@ -106,26 +107,32 @@ let engine = sprites.create(img`
     7 3 
     3 7 
     `, SpriteKind.rocketengine)
-scene.cameraFollowSprite(boom)
-scaling.scaleByPercent(boom, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
-boom.ay = 20
+scene.cameraFollowSprite(apple)
+scaling.scaleByPercent(apple, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+apple.ay = 20
 let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
 let minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
 game.onUpdate(function () {
     fuelSprite.destroy()
-    minimap2.destroy()
     fuelSprite = textsprite.create(convertToText(fuel))
-    fuelSprite.setPosition(boom.x, boom.top)
+    fuelSprite.setPosition(apple.x, apple.top)
+    minimap2.destroy()
     myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
-    minimap.includeSprite(myMinimap, boom, MinimapSpriteScale.MinimapScale)
+    minimap.includeSprite(myMinimap, apple, MinimapSpriteScale.MinimapScale)
     minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
-    minimap2.setPosition(boom.x - 50, boom.y - 30)
-    engine.setPosition(boom.x + -8 * Math.cos(angle), boom.y + -8 * Math.sin(angle))
-    fireball.setPosition(boom.x + -8 * Math.cos(angle), boom.y + -8 * Math.sin(angle))
-    if (focketfleg == 1) {
-        fuel += -2
-        if (boom.y < 400) {
-            landingFlag = 0
-        }
+    minimap2.setPosition(apple.x - 50, apple.y - 30)
+    engine.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
+    fireball.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
+    if (rocketflag == 1) {
+        fuel += -1
+    }
+    if (apple.y < 150) {
+        landingFlag = 0
+    }
+    if (gameOverFlag == 2) {
+        game.over(false)
+    }
+    if (gameOverFlag == 1) {
+        gameOverFlag += 1
     }
 })
